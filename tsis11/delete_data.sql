ca7d80b89f3c82b33cc2bc0bb595a024282b08a7
Saya Zheniskyzy <sayazheniskyzy2005@icloud.com>
2023-05-04 12:54:03 +0600

    upd

--- tsis11/delete_data.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE PROCEDURE delete_data(data_to_delete VARCHAR(20))
-	AS $$	
-	BEGIN
-	DELETE FROM phone_book WHERE name = data_to_delete OR number = data_to_delete;
-	END; $$
-	LANGUAGE plpgsql
